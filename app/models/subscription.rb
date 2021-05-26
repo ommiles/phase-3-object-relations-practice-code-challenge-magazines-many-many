6ade5ce030a0324f5fbd2a582e08450ea6ffaf7d
OM Miles <miles@ommiles.com>
2021-05-26 10:19:05 -0700

    subscription methods added

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,12 @@ class Subscription < ActiveRecord::Base
     
     belongs_to :reader
     belongs_to :magazine
+
+    def reader
+        Reader.find_by(id: self.reader_id)
+    end
+
+    def magazine
+        Magazine.find_by(id: self.magazine_id)
+    end
 end
